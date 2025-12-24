c74d7cde76e404ec2a73fdca8768d9ab95211ce6
unknown <abdulahkotoric123@gmail.com>
2025-12-24 10:30:04 +0100

    PWP-42
    Added Request CRUD and controller

---
+++ template/PawPal.Backend/Market.Application/Modules/Adoptions/AdoptionRequests/Queries/List/ListAdoptionRequestQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawPal.Application.Modules.Adoptions.AdoptionRequests.Queries.List
+{
+    public sealed class ListAdoptionRequestQueryHandler(IAppDbContext context) :
+        IRequestHandler<ListAdoptionRequestQuery,PageResult<ListAdoptionRequestQueryDto>>
+    {
+        public async Task<PageResult<ListAdoptionRequestQueryDto>> Handle(ListAdoptionRequestQuery request,CancellationToken cancellationToken)
+        {
+            var reqList = context.AdoptionRequests.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(request.SearchStatus))
+                reqList = reqList.Where(x => x.Status.ToLower().Contains(request.SearchStatus.ToLower()));
+            reqList.Where(x => x.DateSent == request.SearchDateSent);
+            var finalList = reqList.OrderBy(y => y.DateSent).Select(x => new ListAdoptionRequestQueryDto{
+                DateSent = x.DateSent,
+                Status = x.Status,
+                RequirementId = x.RequirementId,
+            });
+            return await PageResult<ListAdoptionRequestQueryDto>.FromQueryableAsync(finalList, request.Paging, cancellationToken);
+        }
+    }
+}
