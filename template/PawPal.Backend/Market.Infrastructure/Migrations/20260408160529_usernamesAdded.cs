a12a74fb20f39dc4b30c429e454c688b6f8d3301
Iris-Zain Kazazić <iriszain5@gmail.com>
2026-04-08 19:03:21 +0200

    PWP-70: Register bad format FIX, username migration + more

---
+++ template/PawPal.Backend/Market.Infrastructure/Migrations/20260408160529_usernamesAdded.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PawPal.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class usernamesAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Users");
+        }
+    }
+}
